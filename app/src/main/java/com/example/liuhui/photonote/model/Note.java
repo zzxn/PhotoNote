4c71fb484df24ef9afed3a2be8cfd79327975bd4
Zhu xiaoning <1909894639@qq.com>
2018-01-04 18:59:53 +0800

    Reformat

---
+++ app/src/main/java/com/example/liuhui/photonote/model/Note.java
@@ -0,0 +1,83 @@
+package com.example.liuhui.photonote.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.litepal.crud.DataSupport;
+
+public class Note extends DataSupport implements Parcelable {
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            Note note = new Note();
+            note.path = in.readString();
+            note.notebookId = in.readLong();
+            note.id = in.readLong();
+            return note;
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+    /* photo保存的路径 */
+    private String path;
+    /* 笔记所属notebook的id */
+    private long notebookId;
+    private long id;
+
+    /* 构造器 */
+    public Note(String path, long notebookId) {
+        setPath(path);
+        setNotebookId(notebookId);
+    }
+
+    public Note() {
+    }
+
+    protected Note(Parcel in) {
+        path = in.readString();
+        notebookId = in.readLong();
+        id = in.readLong();
+    }
+
+    /* getter and setter */
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getNotebookId() {
+        return notebookId;
+    }
+
+    public void setNotebookId(long notebookId) {
+        this.notebookId = notebookId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(path);
+        dest.writeLong(notebookId);
+        dest.writeLong(id);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/example/liuhui/photonote/model/User.java
@@ -0,0 +1,44 @@
+package com.example.liuhui.photonote.model;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 16307110325 Zhu xiaoning
+ * on 2018/1/3.
+ */
+
+public class User extends DataSupport {
+    private long id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.setId(this.getBaseObjId());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
